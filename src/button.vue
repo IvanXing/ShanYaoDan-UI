c4ddc920de6fd243e0ed38658b4d386f8b424069
xingyifan <xingyifan10@qq.com>
2020-07-31 00:08:56 +0800

    Button 填写 icon 和 iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,27 +1,68 @@
 <template>
-    <button class='s-button'>按钮</button>
+  <button class='s-button' :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href=`#i-${icon}`></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-    export default {}
+    export default {
+        props: ['icon', 'iconPosition']
+    }
 </script>
 
 <style lang="scss">
-    .s-button {
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
-        &:hover {
-            border-color: var(--border-color-hover);
-        }
-        &:active {
-            background-color: var(--button-active-bg);
-        }
-        &:focus {
-            outline: none;
-        }
+  .s-button {
+
+    /*解决居中*/
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    /* inline-flex 造成上下不对齐 所有inline元素都有此问题解决办法*/
+    vertical-align: middle;
+
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+
+    &:hover {
+      border-color: var(--border-color-hover);
     }
+
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+
+    &:focus {
+      outline: none;
+    }
+
+    /*css控制排序*/
+
+    > .icon {
+      order: 1;
+      margin-right: .3em;
+    }
+    > .content {
+      order: 2;
+    }
+
+    &.icon-right {
+      > .icon {
+        order: 2;
+        margin-left: .3em;
+        margin-right: 0;
+      }
+      > .content {
+        order: 1;
+      }
+    }
+  }
 </style>
