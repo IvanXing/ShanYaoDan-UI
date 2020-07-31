8a23876a26f91bc1fa687fa97fc3605fccacb751
Ivan Xing <xingyifan10@qq.com>
2020-07-31 18:01:09 +0800

    创建s-button-group组件

---
+++ src/button-group.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="s-button-group">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+	export default {}
+</script>
+
+<style lang="scss">
+	.s-button-group {
+		display: inline-flex;
+		vertical-align: middle;
+		> .s-button {
+			border-radius: 0;
+			margin-left: -1px; /* 处理重合 */
+
+			/* 处理中间button border-radius */
+
+			&:first-child {
+				border-top-left-radius: var(--border-radius);
+				border-bottom-left-radius: var(--border-radius);
+			}
+			&:last-child {
+				border-top-right-radius: var(--border-radius);
+				border-bottom-right-radius: var(--border-radius);
+			}
+
+			/* 鼠标hover z-index变1  hover border颜色bug解决 相当于浮起*/
+
+			&:hover {
+				position: relative;
+				z-index: 1;
+			}
+		}
+	}
+</style>
