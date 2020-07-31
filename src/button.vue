bd7b78467673730b2c1f0ca4a935fdc0e4d96976
Ivan Xing <xingyifan10@qq.com>
2020-07-31 16:00:10 +0800

    format code

--- src/button.vue
+++ src/button.vue
@@ -1,87 +1,91 @@
 <template>
-  <button class='s-button' :class="{[`icon-${iconPosition}`]: true}">
-    <s-icon class="icon" v-if="icon" :name="icon"></s-icon>
-    <s-icon class="loading" name="loading"></s-icon>
-    <div class="content ">
-      <slot></slot>
-    </div>
-  </button>
+	<button class='s-button' :class="{[`icon-${iconPosition}`]: true}">
+		<s-icon class="icon" v-if="icon" :name="icon"></s-icon>
+		<s-icon class="loading" name="loading"></s-icon>
+		<div class="content ">
+			<slot></slot>
+		</div>
+	</button>
 </template>
 
 <script>
-    export default {
-        // props: ['icon', 'iconPosition']
-        props: {
-            icon: {},
-            iconPosition: {
-                type: String,
-                default: 'left',
-                validator(value) {
-                    return value === 'left' || value === 'right';
-                }
-            }
-        }
-    }
+	export default {
+		// props: ['icon', 'iconPosition']
+		props: {
+			icon: {},
+			iconPosition: {
+				type: String,
+				default: 'left',
+				validator(value) {
+					return value === 'left' || value === 'right';
+				}
+			}
+		}
+	}
 </script>
 
 <style lang="scss">
-  .s-button {
+	.s-button {
 
-    /*loading动画*/
-    @keyframes spin {
-      0% { transform: rotate(0deg); }
-      100% { transform: rotate(360deg); }
-    }
+		/*loading动画*/
+		@keyframes spin {
+			0% {
+				transform: rotate(0deg);
+			}
+			100% {
+				transform: rotate(360deg);
+			}
+		}
 
-    /*解决居中*/
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    /* inline-flex 造成上下不对齐 所有inline元素都有此问题解决办法*/
-    vertical-align: middle;
+		/*解决居中*/
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+		/* inline-flex 造成上下不对齐 所有inline元素都有此问题解决办法*/
+		vertical-align: middle;
 
-    font-size: var(--font-size);
-    height: var(--button-height);
-    padding: 0 1em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
+		font-size: var(--font-size);
+		height: var(--button-height);
+		padding: 0 1em;
+		border-radius: var(--border-radius);
+		border: 1px solid var(--border-color);
+		background: var(--button-bg);
 
-    &:hover {
-      border-color: var(--border-color-hover);
-    }
+		&:hover {
+			border-color: var(--border-color-hover);
+		}
 
-    &:active {
-      background-color: var(--button-active-bg);
-    }
+		&:active {
+			background-color: var(--button-active-bg);
+		}
 
-    &:focus {
-      outline: none;
-    }
+		&:focus {
+			outline: none;
+		}
 
-    /*css控制排序*/
+		/*css控制排序*/
 
-    > .icon {
-      order: 1;
-      margin-right: .3em;
-    }
-    > .content {
-      order: 2;
-    }
+		> .icon {
+			order: 1;
+			margin-right: .3em;
+		}
+		> .content {
+			order: 2;
+		}
 
-    &.icon-right {
-      > .icon {
-        order: 2;
-        margin-left: .3em;
-        margin-right: 0;
-      }
-      > .content {
-        order: 1;
-      }
-    }
+		&.icon-right {
+			> .icon {
+				order: 2;
+				margin-left: .3em;
+				margin-right: 0;
+			}
+			> .content {
+				order: 1;
+			}
+		}
 
-    .loading{
-      animation: spin 2s infinite linear;  /* spin动画 2s 无限 线性 滚动*/
-    }
-  }
+		.loading {
+			animation: spin 2s infinite linear; /* spin动画 2s 无限 线性 滚动*/
+		}
+	}
 </style>
