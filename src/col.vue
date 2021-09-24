a7a64052a63b138d84effa100f6474230a4adcb9
xingyifan <xingyifan10@qq.com>
2021-09-24 11:54:30 +0800

    [row & col] row和col的雏形完成

---
+++ src/col.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'ShanyaodanCol',
+    props: {
+      span: {
+        type: [Number, String]
+      }
+    }
+  }
+</script>
+<style scoped lang="scss">
+.col {
+  height: 100px;
+  background: grey;
+  width: 50%;
+  border: 1px solid red;
+  //scss循环，col-col-1 #{} 是scss的取值操作  对标js ${}
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%;
+    }
+  }
+}
+</style>
---
+++ src/row.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+<style scoped lang="scss">
+.row {
+  display: flex;
+  border: 1px solid red;
+}
+</style>
