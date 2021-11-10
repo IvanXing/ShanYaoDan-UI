a17e4592630f4b3fa00ed28bcdbf37c26f66a7b2
xingyifan <xingyifan10@qq.com>
2021-11-10 11:56:34 +0800

    [col & row] 基本功能

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
+  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+  :style="{marginLeft: gutter/2 + 'px', marginRight: gutter/2 + 'px'}"
+  >
     <slot></slot>
   </div>
 </template>
@@ -9,6 +11,20 @@
     props: {
       span: {
         type: [Number, String]
+      },
+      offset: {
+        type: [Number, String]
+      },
+    },
+    created() {
+      console.log('col created=>', this.$children)
+    },
+    mounted() {
+      console.log('col mounted el=>', this.$children)
+    },
+    data() {
+      return {
+        gutter: 0,
       }
     }
   }
@@ -20,10 +36,18 @@
   width: 50%;
   border: 1px solid red;
   //scss循环，col-col-1 #{} 是scss的取值操作  对标js ${}
+  // 生成 col.col-1
   $class-prefix: col-;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n / 24) * 100%;
+    }
+  }
+  // 循环生成offset空隙
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n / 24) * 100%;
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter/2 + 'px', marginRight: -gutter/2 + 'px',}">
     <slot></slot>
   </div>
 </template>
+<script>
+  export default {
+    name: 'ShanyaodanRow',
+    props: {
+      gutter: {
+        type: [Number, String]
+      }
+    },
+    created() {
+      console.log('row created=>', this.$children)
+    },
+    mounted() {
+      console.log('row mounted el=>', this.$children)
+      this.$children.forEach(vm => {
+        vm.gutter = this.gutter
+      })
+    }
+  }
+</script>
 <style scoped lang="scss">
 .row {
   display: flex;
-  border: 1px solid red;
 }
 </style>
