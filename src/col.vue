c146e1d573fef8f1d6cb96a09c06117264238e61
xingyifan <xingyifan10@qq.com>
2021-11-10 12:25:42 +0800

    [col & row] fix style bug

--- src/col.vue
+++ src/col.vue
@@ -1,40 +1,39 @@
 <template>
   <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-  :style="{marginLeft: gutter/2 + 'px', marginRight: gutter/2 + 'px'}"
+       :style="{paddingLeft: gutter/2 + 'px', paddingRight: gutter/2 + 'px'}"
   >
-    <slot></slot>
+    <div style="border: 1px solid green; height: 100px;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 <script>
-  export default {
-    name: 'ShanyaodanCol',
-    props: {
-      span: {
-        type: [Number, String]
-      },
-      offset: {
-        type: [Number, String]
-      },
+export default {
+  name: 'ShanyaodanCol',
+  props: {
+    span: {
+      type: [Number, String]
     },
-    created() {
-      console.log('col created=>', this.$children)
+    offset: {
+      type: [Number, String]
     },
-    mounted() {
-      console.log('col mounted el=>', this.$children)
-    },
-    data() {
-      return {
-        gutter: 0,
-      }
+  },
+  data() {
+    return {
+      gutter: 0,
     }
+  },
+  created() {
+    console.log('col created=>', this.$children)
+  },
+  mounted() {
+    console.log('col mounted el=>', this.$children)
   }
+}
 </script>
 <style scoped lang="scss">
 .col {
-  height: 100px;
-  background: grey;
   width: 50%;
-  border: 1px solid red;
   //scss循环，col-col-1 #{} 是scss的取值操作  对标js ${}
   // 生成 col.col-1
   $class-prefix: col-;
