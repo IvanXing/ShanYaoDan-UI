01b2cb162a7420fced1a63d606d4e8713c65a7d9
xingyifan <xingyifan10@qq.com>
2021-11-10 12:45:24 +0800

    [col & row] 提取样式到computed

--- src/row.vue
+++ src/row.vue
@@ -1,26 +1,30 @@
 <template>
-  <div class="row" :style="{marginLeft: -gutter/2 + 'px', marginRight: -gutter/2 + 'px',}">
+  <div class="row" :style="rowStyle">
     <slot></slot>
   </div>
 </template>
 <script>
-  export default {
-    name: 'ShanyaodanRow',
-    props: {
-      gutter: {
-        type: [Number, String]
-      }
-    },
-    created() {
-      console.log('row created=>', this.$children)
-    },
-    mounted() {
-      console.log('row mounted el=>', this.$children)
-      this.$children.forEach(vm => {
-        vm.gutter = this.gutter
-      })
+export default {
+  name: 'ShanyaodanRow',
+  props: {
+    gutter: {
+      type: [Number, String]
     }
+  },
+  computed: {
+    rowStyle() {
+      let {gutter} = this
+      return {
+        marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'
+      }
+    }
+  },
+  mounted() {
+    this.$children.forEach(vm => {
+      vm.gutter = this.gutter
+    })
   }
+}
 </script>
 <style scoped lang="scss">
 .row {
